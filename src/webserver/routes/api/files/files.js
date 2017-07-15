26fd6e3d89457fc99ebf938f1a37f4f417f7dc5f
kevindoveton <kevindoveton@me.com>
2017-07-15 17:24:51 +0930

    move build slide to function

--- src/webserver/routes/api/files/files.js
+++ src/webserver/routes/api/files/files.js
@@ -1,12 +1,12 @@
-String.prototype.trim = function() {
-	return this.replace(/^\s+|\s+$/g,"");
-}
-String.prototype.ltrim = function() {
-	return this.replace(/^\s+/,"");
-}
-String.prototype.rtrim = function() {
-	return this.replace(/\s+$/,"");
-}
+// String.prototype.trim = function() {
+// 	return this.replace(/^\s+|\s+$/g,"");
+// }
+// String.prototype.ltrim = function() {
+// 	return this.replace(/^\s+/,"");
+// }
+// String.prototype.rtrim = function() {
+// 	return this.replace(/\s+$/,"");
+// }
 
 function copyObj(obj) {
   return JSON.parse(JSON.stringify(obj));
@@ -21,12 +21,6 @@ const mongoose = require('mongoose');
 
 const Presentation = mongoose.model('Presentation');
 const Template = mongoose.model('Template')
-
-var template = {};
-
-Template.findOne({}, (err, t) => {
-  template = t;
-})
 
 router.get('/', (req, res) => {
   Presentation.find((err, templates) => {
@@ -60,60 +54,14 @@ router.post('/', (req, res) => {
     return res.status(400).send('request format invalid');
   }
   
-  var doc = undefined;
-  
-  doc = JSON.parse(fs.readFileSync('./src/tempPP/Document.json', "utf-8"));
-  d = doc['RVPresentationDocument'];
-  
-  // format the document from template
-  d['$'].height = (template.preview.container.height).slice(0,-2);
-  d['$'].width = (template.preview.container.width).slice(0,-2);
-  
-  const BASE_SLIDE = JSON.parse(template.slide);
-
-  var rtfStart = ProPresenter.decode(BASE_SLIDE['displayElements'][0]['RVTextElement'][0]['$']['RTFData']);
-  var lastControlStart = rtfStart.lastIndexOf('\\cf');
-  var pos = rtfStart.indexOf(' ', lastControlStart);
-
-  rtfStart = rtfStart.substring(0, pos);
-  
-  slidesGroup = [];
-  
-  for (var i = 0; i < req.body.slides.length; i++) {
-
-    const slide = req.body.slides[i];
-    let rtfData = ProPresenter.ConvertHtmlToRtf(slide.htmlContent);
-
-    let proSlide = copyObj(BASE_SLIDE);
-    rtfData = rtfStart + rtfData + '}';
-    rtfEncoded = ProPresenter.encode(rtfData);
-
-    proSlide['$']['sort_index'] = i;
-    proSlide['$']['serialization-array-index'] = i;
-    proSlide['$']['label'] = 'slide ' + i;
-    proSlide['$']['UUID'] = '5657B2D7-7DA3-0640-2069-6644056432C' + i;
-    proSlide['displayElements'][0]['RVTextElement'][0]['$']['RTFData'] = rtfEncoded;
-    
-    slidesGroup.push(proSlide);
-  }
-  
-  d['groups'][0]['RVSlideGrouping'][0]['slides'][0]['RVDisplaySlide'] = slidesGroup;
-
-  let p = new Presentation({
-    slide: XmlParser.build(doc),
-    title: req.body.title,
-    date: req.body.date
-  });
-  p.save((err) => {
-    if (err) {
-      return res.status(500).send({
-        success: false
-      });
-    }
-    return res.status(201).json({
-      success: true
-    });
-  });
+  Template.find({_id: req.body.template._id}, (err, _template) => {
+    BuildPro5Document(req.body, _template[0]).then(() => {
+      return res.status(201).send();
+    }).catch((err) => {
+      console.log(err);
+      return res.status(500).send(err);
+    })
+  })
 
 })
 
@@ -131,5 +79,68 @@ router.delete('/:template_id', (req, res) => {
 	});
   
 });
+
+
+/**
+ * build a propresenter document and save in mongo
+ * @param file {Object} - the sermon to build
+ * @param template {Object} - the template to use
+ * @returns Promise
+*/
+function BuildPro5Document(file, template) {
+  return new Promise((resolve, reject) => {
+    // TODO: this should probably happen async
+    var doc = JSON.parse(fs.readFileSync('./src/tempPP/Document.json', "utf-8"));
+    var d = doc['RVPresentationDocument'];
+    
+    // format the document from template
+    d['$'].height = (template.preview.container.height).slice(0,-2);
+    d['$'].width = (template.preview.container.width).slice(0,-2);
+    
+    const BASE_SLIDE = JSON.parse(template.slide);
+
+    var rtfStart = ProPresenter.decode(BASE_SLIDE['displayElements'][0]['RVTextElement'][0]['$']['RTFData']);
+    var lastControlStart = rtfStart.lastIndexOf('\\cf');
+    var pos = rtfStart.indexOf(' ', lastControlStart);
+
+    rtfStart = rtfStart.substring(0, pos);
+    
+    slidesGroup = [];
+    
+    for (var i = 0; i < file.slides.length; i++) {
+
+      const slide = file.slides[i];
+      let rtfData = ProPresenter.ConvertHtmlToRtf(slide.htmlContent);
+
+      let proSlide = copyObj(BASE_SLIDE);
+      rtfData = rtfStart + rtfData + '}';
+      rtfEncoded = ProPresenter.encode(rtfData);
+
+      proSlide['$']['sort_index'] = i;
+      proSlide['$']['serialization-array-index'] = i;
+      proSlide['$']['label'] = 'slide ' + i;
+      proSlide['$']['UUID'] = '5657B2D7-7DA3-0640-2069-6644056432C' + i;
+      proSlide['displayElements'][0]['RVTextElement'][0]['$']['RTFData'] = rtfEncoded;
+      
+      slidesGroup.push(proSlide);
+    }
+    
+    d['groups'][0]['RVSlideGrouping'][0]['slides'][0]['RVDisplaySlide'] = slidesGroup;
+
+    let p = new Presentation({
+      slide: XmlParser.build(doc),
+      title: file.title,
+      date: file.date
+    });
+    
+    p.save((err) => {
+      if (err) {
+        reject(err);
+      }
+      resolve();
+    });
+  })
+  
+}
 
 module.exports = router;
