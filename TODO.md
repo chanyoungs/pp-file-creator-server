ae7fb3bace8d08bcbfd52bb94411495484943867
kevindoveton <kevindoveton@me.com>
2017-07-15 20:49:55 +0930

    add a few more file manipulation paths

--- TODO.md
+++ TODO.md
@@ -5,7 +5,7 @@
 | src/webserver/routes/api/api.js | 1 | figure out cors
 | src/webserver/routes/api/api.js | 41 | DELETE
 | src/webserver/routes/api/files/files.js | 92 | this should probably happen async
+| src/webserver/routes/api/templates/templates.js | 85 | check we have memory - object isnt huge etc
 | src/webserver/routes/api/users/users.js | 23 | DELETE THIS!
 | src/webserver/routes/api/users/users.js | 45 | check the user doesn't already exist
 | src/webserver/routes/api/users/users.js | 60 | should probably delete this
-| src/webserver/routes/api/templates/templates.js | 85 | check we have memory - object isnt huge etc
--- src/webserver/routes/api/s3/s3.js
+++ src/webserver/routes/api/s3/s3.js
@@ -9,6 +9,12 @@ const S3 = mongoose.model('S3');
 const path = require('path');
 // const fs = require('fs');
 
+router.get('/', (req, res) => {
+  S3.find((err, s3) => {
+    res.json(s3);
+  })
+})
+
 router.get('/:file_id', (req, res) => {
   S3.find({_id: req.params.file_id}, (err, doc) => {
     if (err) {
@@ -21,13 +27,21 @@ router.get('/:file_id', (req, res) => {
   })
 })
 
+router.delete('/all', (req, res) => {
+  S3.find().remove().exec();
+  res.send();
+});
+
+router.delete('/:file_id', (req, res) => {
+  S3.find({_id: req.params.file_id}).remove().exec();
+  res.send();
+});
+
 router.post('/', upload.single('file'), (req, res) => {
   if (typeof(req.file) == 'undefined') {
     return res.status(400).send('send file, also check content-type');
   }
-  
-  // var filePath = path.join('/files/', req.file.path);
-  
+
   var s3 = new S3({
     user: 'testy',
     path: req.file.path,
